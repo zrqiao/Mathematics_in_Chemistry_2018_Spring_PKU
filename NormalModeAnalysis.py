06fe40ff6958b42a8f82c3192f3613ca179bd080
Utena <utenaq@pku.edu.cn>
2018-04-02 18:01:07 +0800

    Initial Commit

---
+++ NormalModeAnalysis.py
@@ -0,0 +1,8 @@
+import numpy
+import math
+raw_hessian = open('hess.dat')
+mass = list[1.0008, 15.9997, 1.0008]
+hess = list(map(lambda line: list(map(float,
+    [num for num in line.split()])), raw_hessian.readlines()))
+for i, j in range(9):
+    hess[i][j] = hess[i][j]/(math.sqrt(mass(math.floor(i/3)))*math.sqrt(mass(math.floor(j/3))))
