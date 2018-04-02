bd36799a28f237d185b54d9e29a8b576e18c1215
Utena <utenaq@pku.edu.cn>
2018-04-02 18:14:47 +0800

    Update

--- NormalModeAnalysis.py
+++ NormalModeAnalysis.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import math
 raw_hessian = open('hess.dat')
 mass = list[1.0008, 15.9997, 1.0008]
@@ -6,3 +6,7 @@ hess = list(map(lambda line: list(map(float,
     [num for num in line.split()])), raw_hessian.readlines()))
 for i, j in range(9):
     hess[i][j] = hess[i][j]/(math.sqrt(mass(math.floor(i/3)))*math.sqrt(mass(math.floor(j/3))))
+freq, mode=np.linalg.eig(hess)
+
+
+formatNum="{:.8f}".format(Coor).append("   ")
