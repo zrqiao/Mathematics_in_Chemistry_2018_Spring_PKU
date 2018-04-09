0f51df31ee1103fecdea519a2265711340698e62
Jarren Qiao <utenaq@pku.edu.cn>
2018-04-09 21:17:10 +0800

    NormalMode

--- Assignment1 Normal_Mode_Analysis/NormalModeAnalysis.py
+++ Assignment1 Normal_Mode_Analysis/NormalModeAnalysis.py
@@ -18,11 +18,15 @@ for i in range (3):
     temp=iniCoor.readline().split()
     for j in range (3):
         Coor0[3*i+j]=float(temp[j+1])
+FreqW=open('Frequencies.dat','w')
 for i in range(9):
     if freq2[i]>0:
         freq[i]=math.sqrt(freq2[i])
+        freqr=freq[i]/(2*math.pi)/(137*5.2917721*10**(-9)*math.sqrt(1836))
+        FreqW.write(str(freqr)+'\n')
         Coor=[[Coor0[j]+L*mode[j][i]*math.sin(freq[i]*0.05*t)/(math.sqrt(mass[j])) for j in range(9)] for t in range (500)]
         traj=open('materials/traj'+str(i)+'.mdcrd','w')
         for t in range(500):
             traj.write('   '.join(map(lambda x: "{:.8f}".format(x),Coor[t]))+'\n')
         traj.close()
+FreqW.close()
